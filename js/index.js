f0bd653aa866f3b7c6f60e0b2d62766629c57285
Justin Gardner <the1jscott@gmail.com>
2020-08-18 15:12:32 -0500

    fixed code and created a thrid button

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 const myButton = document.getElementById('clickMe');
 const noun1 = document.getElementById('noun1');
 const noun1Input = document.getElementById('noun1Input');
-
+const noun2Input = document.getElementById('noun2Input');
+const verb1Input = document.getElementById('verb1Input');
 myButton.addEventListener('click', function (event) {
   event.preventDefault();
-
+  
   noun1.innerHTML = noun1Input.value;
+  noun2.innerHTML = noun2Input.value;
+  verb1.innerHTML = verb1Input.value;
 });
